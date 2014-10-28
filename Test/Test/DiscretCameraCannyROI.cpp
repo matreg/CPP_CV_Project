391a2f5f8c3df5bd95dc6f671e7f4d7f04906116
Sasha <matreg2012>
2014-10-28 20:21:32 +0200

    маштабування
    маштабування області 320*240 в 640*480

--- Test/Test/DiscretCameraCannyROI.cpp
+++ Test/Test/DiscretCameraCannyROI.cpp
@@ -9,20 +9,27 @@ int main(int argc, char* argv[])
 		CvCapture* capture = cvCreateCameraCapture(0); //cvCaptureFromCAM( 0 );
         assert( capture );
         IplImage* frame=0;
+		IplImage* framex2=0;
 		IplImage* gray = 0;
 		IplImage* dst = 0;
 
         cvNamedWindow("capture", CV_WINDOW_AUTOSIZE);
+		//cvNamedWindow("capture2", CV_WINDOW_AUTOSIZE);
 		cvNamedWindow("gray",CV_WINDOW_AUTOSIZE);
         cvNamedWindow("cvCanny",CV_WINDOW_AUTOSIZE);
 
-        while(true){
-                // получаем кадр
-                frame = cvQueryFrame( capture );
+		int x = (int) (cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_WIDTH) / 4);
+		int y = (int) (cvGetCaptureProperty(capture, CV_CAP_PROP_FRAME_HEIGHT) / 4);
 
-                // показываем
-                cvShowImage("capture", frame);
-        
+		frame = cvQueryFrame( capture );
+		cvSetImageROI(frame, cvRect(x,y,320,240));
+		framex2 = cvCreateImage(cvSize(frame->width, frame->height),IPL_DEPTH_8U, 1 );
+
+
+        while(true){
+ 
+				frame = cvQueryFrame( capture );
+
                 char c = cvWaitKey(33);
                 if (c == 27) { // нажата ESC
                         break;
@@ -30,12 +37,17 @@ int main(int argc, char* argv[])
 				gray = cvCreateImage( cvGetSize(frame), IPL_DEPTH_8U, 1 );
 				dst = cvCreateImage( cvGetSize(frame), IPL_DEPTH_8U, 1 );
 				cvCvtColor(frame, gray, CV_RGB2GRAY);
-				cvCanny(gray, dst, 10, 90, 5);
-
+				cvCanny(gray, dst, 10, 120, 3);
+				//cvResize
 				cvShowImage("capture",frame);
 				cvShowImage("gray",gray);
 				cvShowImage("cvCanny", dst );
-                
+
+				cvResize(dst, framex2, 3);
+                cvShowImage("capture2", framex2);
+				
+				cvReleaseImage(&gray);
+				cvReleaseImage(&dst);
         }
         // освобождаем ресурсы
         cvReleaseCapture(&capture );
